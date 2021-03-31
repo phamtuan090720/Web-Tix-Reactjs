401b9b2c80fa9244e15243b495f30c88fc686b14
Phạm Tuân <52484769+phamtuan090720@users.noreply.github.com>
2021-03-31 22:14:36 +0700

    up

--- src/api/apiPost.js
+++ src/api/apiPost.js
@@ -1,12 +1,6 @@
 import axios from "axios";
 const createConnector = () => {
   let config = {};
-  // if(JSON.parse(sessionStorage.getItem("USER"))){
-  //   config.headers={
-  //      Authorization: `Bearer ${JSON.parse(sessionStorage.getItem("USER")).accessToken}`
-  //   }
-  // }
-  // console.log("APIPOSt",config);
   return axios.create(config);
 };
 const connector = createConnector();
