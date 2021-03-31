9aacd8ab61595bcc66a5708b42ddaf44b8afa5fc
Phạm Tuân <52484769+phamtuan090720@users.noreply.github.com>
2021-03-31 22:41:12 +0700

    up

--- src/components/CarouselMovie/dataBanner.js
+++ src/components/CarouselMovie/dataBanner.js
@@ -1,18 +1,18 @@
- import img1 from "../../img/Carousel/dong-gia-1k-ve-khi-mua-ve-qua-tix-16011908558560.jpg";
- import img2 from '../../img/Carousel/rom-c18-16008300686919.png';
-
+ import Rom from "../../img/Carousel/dong-gia-1k-ve-khi-mua-ve-qua-tix-16011908558560.jpg";
+ import BoGia from '../../img/Carousel/bo-gia-16146819941008.png';
+ import AmQuy from '../../img/Carousel/an-quy-16170973985377.png';
  const data = [
     {
-        banner:img1,
+        banner:Rom,
         trailler:"https://www.youtube.com/embed/XRm1P7oGpMQ"
     },
     {
-        banner:img2,
-        trailler:"https://www.youtube.com/embed/XRm1P7oGpMQ"
+        banner:BoGia,
+        trailler:"https://youtu.be/jluSu8Rw6YE"
     },
     {
-        banner:img2,
-        trailler:"https://www.youtube.com/embed/XRm1P7oGpMQ"
+        banner:AmQuy,
+        trailler:"https://youtu.be/98W8bv_8Kz8"
     }
 ] 
 export default data;
