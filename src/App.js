00d53719378f3e2e3868a5e586317524c16f8ed3
Phạm Tuân <52484769+phamtuan090720@users.noreply.github.com>
2021-03-31 22:11:29 +0700

    up

--- src/App.js
+++ src/App.js
@@ -49,23 +49,3 @@ function App() {
 }
 
 export default App;
-// import { Route, Switch, withRouter } from "react-router-dom";
-// import React, { Component } from 'react'
-// import { routesHome, routesAdmin } from "./routes";
-// import HomeTemplate from "./container/HomeTemplate";
-// export default class App extends Component {
-//   showLayoutHome = (routes) => {
-//     if (routes && routes.length > 0) {
-//       return routes.map((item, index) => {
-//         return <HomeTemplate key={index} exact={item.exact} path={item.path} Component={item.component} />
-//       });
-//     }
-//   }
-//   render() {
-//     return (
-//       <Switch>
-//         {this.showLayoutHome(routesHome)}
-//       </Switch>
-//     )
-//   }
-// }
