1241dbf23fe4a307ce5536eacdd36be32b708332
Phạm Tuân <52484769+phamtuan090720@users.noreply.github.com>
2021-04-01 12:44:46 +0700

    up

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/css/bootstrap.min.css";
-// import 'owl.carousel/dist/assets/owl.carousel.min.css';
-// import 'owl.carousel/dist/assets/owl.theme.default.min.css';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "jquery/dist/jquery.min.js";
